69f4826694bbfb999f2e1ac6ec83d9b8d575775f
Nicola <nicola@jumpingrivers.com>
2022-06-29 10:48:29 +0100

    update reactivity

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 server = function(input, output) {
 
+
   # load static html for treeview
   output$treeview = shiny::renderUI({
     shiny::div(
@@ -11,21 +12,83 @@ server = function(input, output) {
     )
   })
 
-  # download plot button
-  output$download_plot = downloadHandler(
+  # list of all files
+  all_files = shiny::reactive({
+    all_files = tibble::as_tibble(
+      list.files(
+        glue::glue("www/data/wcdemo/scanner_output/{input$cluster_id}")
+      )
+    )
+    all_files = all_files %>%
+      mutate(filetype = sub(".*\\.", "", value))
+    return(all_files)
+  })
+
+
+# Tables Tab --------------------------------------------------------------
+
+  # drop down for tables
+  output$choose_table = shiny::renderUI({
+    all_tables = all_files() %>%
+      filter(filetype %in% c("csv", "CSV")) %>%
+      pull(value)
+    shiny::selectInput("table_type",
+                       label = "Select table type:",
+                       choices = all_tables)
+  })
+
+  # display table
+  table_file = shiny::reactive({
+    table_file = glue::glue("www/data/wcdemo/scanner_output/{input$cluster_id}/{input$table_type}")
+    return(table_file)
+  })
+
+  output$display_table = shiny::renderUI({
+    table_to_display= suppressMessages(readr::read_csv(table_file()))
+    reactable::reactable(table_to_display)
+  })
+
+  # download table button
+  output$download_table = shiny::downloadHandler(
     filename = function() {
-      paste("plot-", Sys.Date(), ".png", sep = "")
+      glue::glue("{input$cluster_id}_{input$table_type}")
     },
-    content = function(con) {
+    content = function(file) {
+      file.copy(table_file(), file)
     }
   )
 
-  # download table button
-  output$download_table = downloadHandler(
+# Plots Tab ---------------------------------------------------------------
+
+  # drop down for plots
+  output$choose_plot = shiny::renderUI({
+    all_images = all_files() %>%
+      filter(filetype %in% c("png", "PNG")) %>%
+      pull(value)
+    shiny::selectInput("plot_type",
+                       label = "Select plot type:",
+                       choices = all_images)
+  })
+
+  # display plot
+  plot_file = shiny::reactive({
+    plot_file = glue::glue("www/data/wcdemo/scanner_output/{input$cluster_id}/{input$plot_type}")
+    return(plot_file)
+  })
+
+  output$display_plot = shiny::renderUI({
+    src = substring(plot_file(), 5)
+    shiny::img(src = src,
+               width = "400px")
+  })
+
+  # download plot button
+  output$download_plot = shiny::downloadHandler(
     filename = function() {
-      paste("table-", Sys.Date(), ".csv", sep = "")
+      glue::glue("{input$cluster_id}_{input$plot_type}")
     },
-    content = function(con) {
+    content = function(file) {
+      file.copy(plot_file(), file)
     }
   )
 
--- ui.R
+++ ui.R
@@ -1,5 +1,3 @@
-cluster_id = 1601
-
 library(dplyr)
 all_clusters = tibble::as_tibble(
   list.files(
@@ -31,11 +29,11 @@ ui = shiny::navbarPage(
           shiny::fluidRow(shiny::column(
             6,
             align = "center",
-            shiny::htmlOutput("treeview"),
+            #shiny::htmlOutput("treeview"),
             style = "height:600px;"
           ))
         ),
-        # search bar for mutations
+        # search bar for mutations (example only)
         shiny::br(),
         shiny::selectizeInput(inputId = "mutation",
                               label = "Search for mutation",
@@ -45,7 +43,7 @@ ui = shiny::navbarPage(
       ),
 
 
-# Right hand side - show outputs ------------------------------------------
+      # Right hand side - show outputs ------------------------------------------
       shiny::column(6,
         # choose cluster id
         shiny::selectInput(inputId = "cluster_id",
@@ -53,63 +51,50 @@ ui = shiny::navbarPage(
                            choices = all_clusters),
         # output options
         shiny::tabsetPanel(id = "plot_tabs",
-                           # Tables tab
-                           shiny::tabPanel("Tables",
-                                           # drop down menu to select plot
-                                           shiny::br(),
-                                           shiny::selectInput("table_type",
-                                                              "Select table type:",
-                                                              c("Table 1" = "table1",
-                                                                "Table 2" = "table2")),
-                                           # display table
-                                           shiny::wellPanel(
-                                             shiny::fluidRow(shiny::column(
-                                               12,
-                                               reactable::reactable(
-                                                 suppressMessages(
-                                                   readr::read_csv(
-                                                     "www/data/wcdemo/scanner_output/1603/cocirculating_lineages.csv"
-                                                   )
-                                                 )
-                                               ),
-                                               align = "center",
-                                               style = "height:400px;"))
-                                           ),
-                                           # download button to download current table
-                                           shiny::br(),
-                                           fluidRow(
-                                             column(12, align = "center",
-                                                    downloadButton("download_table",
-                                                                   label = "Download"))
-                                           )
+           # Tables tab
+           shiny::tabPanel("Tables",
+                           # drop down menu to select plot
+                           shiny::br(),
+                           shiny::uiOutput("choose_table"),
+                           # display table
+                           shiny::wellPanel(
+                             shiny::fluidRow(shiny::column(
+                               12,
+                               shiny::uiOutput("display_table"),
+                               align = "center",
+                               style = "height:400px;"))
                            ),
-
-                           # Plots tab
-                           shiny::tabPanel("Plots",
-                                           # drop down menu to select plot
-                                           shiny::br(),
-                                           shiny::selectInput("plot_type",
-                                                              "Select plot type:",
-                                                              c("Type 1" = "type1",
-                                                                "Type 2" = "type2")),
-                                           # display plot
-                                           shiny::wellPanel(
-                                             shiny::fluidRow(shiny::column(
-                                               12,
-                                               align = "center",
-                                               shiny::img(src = "data/wcdemo/scanner_output/1602/frequency.png",
-                                                          width = "400px"),
-                                               style = "height:400px;"))
-                                           ),
-                                           # download button to download current plot
-                                           shiny::br(),
-                                           fluidRow(
-                                             column(12, align = "center",
-                                                    downloadButton("download_plot",
-                                                                   label = "Download"))
-                                           )
+                           # download button to download current table
+                           shiny::br(),
+                           shiny::fluidRow(
+                             shiny::column(12, align = "center",
+                                    shiny::downloadButton("download_table",
+                                                          label = "Download"))
                            )
-      )) # end right column
+           ),
+           # Plots tab
+           shiny::tabPanel("Plots",
+                           # drop down menu to select plot
+                           shiny::br(),
+                           shiny::uiOutput("choose_plot"),
+                           # display plot
+                           shiny::wellPanel(
+                             shiny::fluidRow(shiny::column(
+                               12,
+                               align = "center",
+                               shiny::uiOutput("display_plot"),
+                               style = "height:400px;"))
+                           ),
+                           # download button to download current plot
+                           shiny::br(),
+                           shiny::fluidRow(
+                             shiny::column(12, align = "center",
+                                    shiny::downloadButton("download_plot",
+                                                          label = "Download"))
+                           )
+           )
+        )
+      ) # end right column
     ) # end fluid row
   ), # end "data" page
 
