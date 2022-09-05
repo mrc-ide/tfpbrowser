054bc7f96491f89f22ee3e169d2ac00517a3a0a7
Nicola <nicola@jumpingrivers.com>
2022-09-05 10:55:01 +0100

    add initial value test

--- tests/testthat/test-app-function.R
+++ tests/testthat/test-app-function.R
@@ -1,8 +1,12 @@
 test_that("tfpbrowser app initial values are consistent", {
-  # Don't run these tests on the CRAN build servers
-  skip_on_cran()
-
-  shiny_app <- tfpbrowser::run_app()
-  app <- shinytest2::AppDriver$new(shiny_app, name = "tfpbrowser")
-  app$expect_values()
+  if (interactive()) {
+    shiny_app <- tfpbrowser::run_app()
+    app <- shinytest2::AppDriver$new(shiny_app,
+                                     name = "tfpbrowser")
+    navbar_tab_items = app$get_text(".nav-item")
+    observed_navbar_tab_items = stringr::str_squish(navbar_tab_items)
+    expected_navbar_tab_items = c("Data", "About", "Tables", "Plots")
+    testthat::expect_equal(observed_navbar_tab_items, expected_navbar_tab_items)
+  }
 })
+
