69010f01dca8eae48f6c4c12a1cccc3a42f78506
Nicola <nicola@jumpingrivers.com>
2022-09-08 11:37:02 +0100

    fix linting

--- tests/testthat/test-app-function.R
+++ tests/testthat/test-app-function.R
@@ -1,7 +1,7 @@
 test_that("tfpbrowser app initial values are consistent", {
   if (interactive()) {
-    shiny_app <- tfpbrowser::run_app()
-    app <- shinytest2::AppDriver$new(shiny_app,
+    shiny_app = tfpbrowser::run_app()
+    app = shinytest2::AppDriver$new(shiny_app,
                                      name = "tfpbrowser")
     navbar_tab_items = app$get_text(".nav-item")
     observed_navbar_tab_items = stringr::str_squish(navbar_tab_items)
@@ -9,4 +9,3 @@ test_that("tfpbrowser app initial values are consistent", {
     testthat::expect_equal(observed_navbar_tab_items, expected_navbar_tab_items)
   }
 })
-
