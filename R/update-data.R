6a2602ecc8fa79228b74321c6057b5e97d372789
Nicola <nicola@jumpingrivers.com>
2023-01-04 11:20:58 +0000

    simplify function

--- R/update-data.R
+++ R/update-data.R
@@ -7,26 +7,12 @@ empty_treeview = function(treeview = "tree-logistic_growth_rate.rds") {
                          package = "tfpbrowser",
                          mustWork = TRUE)
   g = readRDS(filename)
-  built = suppressWarnings(ggplot2::ggplot_build(g))
-  plot_data = built$data[3][[1]]
-
-  # get tooltips here as well to add back in
-
   new_g = g +
-    ggplot2::guides(colour = "none") +
-    ggnewscale::new_scale("size") +
-    ggnewscale::new_scale("shape") +
-    ggplot2::geom_point(data = plot_data, # make this interactive?
-                        mapping = ggplot2::aes(x = .data$x,
-                                               y = .data$y,
-                                               size = .data$size,
-                                               shape = .data$shape),
-                        colour = "grey") +
-    ggplot2::scale_size_identity(guide = "none") +
-    ggplot2::scale_shape_identity(guide = "none")
+    ggplot2::guides(colour = "none",
+                    fill = "none",
+                    shape = "none") +
+    ggplot2::labs(title = "Colour: mutation")
   new_filename = file.path("inst", "app", "www", "data", "treeview",
                            "tree-mutations.rds")
-  # need to add another layer / in cluster id
-  # (if treeview mutation, use second last element to get cluster id)
   saveRDS(new_g, file = new_filename)
 }
