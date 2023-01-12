63d85d8904be7880a2030ccebb592f9aa74e342d
Nicola <nicola@jumpingrivers.com>
2023-01-12 01:44:43 +0000

    add tooltip parse function back for now

--- R/utils.R
+++ R/utils.R
@@ -151,6 +151,18 @@ downloader_tab_panel = function(title,
 }
 
 #' function to get node id from data_id column of ggplot
+#' TO BE REMOVED AFTER TOOLTIPS TFPSCANNER PR IS MERGED
+#' @param tooltip_input Character vector of tooltip content
+#' @export
+get_cluster_ID = function(tooltip_input) {
+  # start searching the string after the "Cluster.ID" text
+  # until the next new line
+  match_matrix = stringr::str_match(tooltip_input, pattern = r"(Cluster.ID\s+#(\d+))")
+  cluster_ids = as.numeric(match_matrix[, 2])
+  return(cluster_ids)
+}
+
+#' function to get node id from data_id column of ggplot
 #' @param widgetChoice From click of radio button to select widget to display
 #' @param treeviewSelected Output from clicking on treeview plot
 get_selected_cluster_id = function(widgetChoice,
@@ -164,12 +176,15 @@ get_selected_cluster_id = function(widgetChoice,
                           "sina-simple_logistic_growth_rate.rds")) {
     ids = built$data[1][[1]]["data_id"]
     tooltips = built$data[1][[1]]$tooltip
+    ###### MOVE THIS AFTER TRIMMED TOOLTIPS PR IS MERGED
+    ###### SINA PLOTS CURRENTLY STILL USE LONGER TOOLTIPS
+    tooltip_ids = get_cluster_ID(tooltips)
   } else {
     n_layers = length(built$data)
     ids = built$data[n_layers][[1]]["data_id"]
     tooltips = built$data[n_layers][[1]]$tooltip
+    tooltip_ids = suppressWarnings(readr::parse_number(tooltips))
   }
-  tooltip_ids = suppressWarnings(readr::parse_number(tooltips))
   ids$cluster_ids = tooltip_ids
   selected_cluster = as.numeric(ids[which(ids$data_id == treeviewSelected), 2])
   return(selected_cluster)
