fe75c6a9872f82e04e8f4fdc69e28682f3cd2c1d
Russ Hyde <russ@jumpingrivers.com>
2024-03-11 15:19:53 +0000

    refac: move creation of interactive ggiraph graphics into a function

--- R/app_server.R
+++ R/app_server.R
@@ -36,52 +36,20 @@ app_server = function(input, output, session) {
   # create ggiraph output from saved ggplot2 outputs
   output$treeview = ggiraph::renderGirafe({
     shiny::req(input$widgetChoice)
-    # define tooltip
-    tooltip_css = paste0(
-      "background-color:black;",
-      "color:grey;",
-      "padding:14px;",
-      "border-radius:8px;",
-      "font-family:\"Courier New\",monospace;"
-    )
-    # set options
-    if (input$widgetChoice == "tree-mutations.rds") {
-      girafe_options = list(
-        ggiraph::opts_selection(css = "fill:red;"),
-        ggiraph::opts_selection_inv(css = "fill:grey;"),
-        ggiraph::opts_sizing(rescale = FALSE),
-        ggiraph::opts_zoom(max = 5),
-        ggiraph::opts_tooltip(
-          css = tooltip_css,
-          use_fill = FALSE
-        )
-      )
-    } else {
-      girafe_options = list(
-        ggiraph::opts_selection(type = "single"),
-        ggiraph::opts_sizing(rescale = FALSE),
-        ggiraph::opts_zoom(max = 5),
-        ggiraph::opts_tooltip(
-          css = tooltip_css,
-          use_fill = FALSE
-        )
-      )
-    }
+
     # set size
     w = shinybrowser::get_width() / 72
     h = (1800 - 40) / 72
-    # make tree
-    suppressWarnings(
-      ggiraph::girafe(
-        ggobj = imported_ggtree(),
-        width_svg = w,
-        height_svg = h,
-        options = girafe_options
-      )
+
+    create_girafe(
+      ggobj = imported_ggtree(),
+      widget_choice = input$widgetChoice,
+      width_svg = w,
+      height_svg = h,
+      suppress_warnings = TRUE
     )
   }) %>%
     shiny::bindCache(input$widgetChoice)
-
 
   # Mutation colouring ------------------------------------------------------
 
